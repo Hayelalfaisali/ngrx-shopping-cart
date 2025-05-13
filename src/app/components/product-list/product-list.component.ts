08a190350f898fd770d0cc430f4a743b840541fc
Hayelalfaisali <hayelalfasly@gmail.com>
2025-05-13 10:14:30 +0300

    updated the design of the products card

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -17,21 +17,42 @@ export class ProductListComponent {
       id: 1,
       name: 'Perfume A',
       price: 50,
-      image: 'https://images.pexels.com/photos/9659895/pexels-photo-9659895.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      image: 'https://smartfox.co.nz/cdn/shop/files/28362980706_TBKUOBDPK2SW.jpg?v=1725144018&width=1214',
       description: 'A refreshing floral scent perfect for daytime wear, with notes of jasmine and citrus.'
     },
     {
       id: 2,
       name: 'Perfume B',
       price: 80,
-      image: 'https://images.pexels.com/photos/9659894/pexels-photo-9659894.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      image: 'https://i.ebayimg.com/images/g/R8IAAOSw1gxnjobY/s-l1200.jpg',
       description: 'A deep and warm fragrance with hints of vanilla, musk, and cedarwood.'
     },
     {
       id: 3,
       name: 'Perfume C',
       price: 120,
-      image: 'https://images.pexels.com/photos/9659892/pexels-photo-9659892.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      image: 'https://leoncechenal.com/wp-content/uploads/2022/05/iconic_french_perfumes.webp',
+      description: 'A luxurious blend of spices and rare blossoms, designed for special evenings.'
+    },
+    {
+      id: 4,
+      name: 'Perfume A',
+      price: 50,
+      image: 'https://smartfox.co.nz/cdn/shop/files/28362980706_TBKUOBDPK2SW.jpg?v=1725144018&width=1214',
+      description: 'A refreshing floral scent perfect for daytime wear, with notes of jasmine and citrus.'
+    },
+    {
+      id: 5,
+      name: 'Perfume B',
+      price: 80,
+      image: 'https://i.ebayimg.com/images/g/R8IAAOSw1gxnjobY/s-l1200.jpg',
+      description: 'A deep and warm fragrance with hints of vanilla, musk, and cedarwood.'
+    },
+    {
+      id: 6,
+      name: 'Perfume C',
+      price: 120,
+      image: 'https://leoncechenal.com/wp-content/uploads/2022/05/iconic_french_perfumes.webp',
       description: 'A luxurious blend of spices and rare blossoms, designed for special evenings.'
     }
   ];
